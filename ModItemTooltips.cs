dfb9f4aaf3dd77c9c186165d33db2b1c6ff05dfa
gorateron <zond1600@student.nhl.nl>
2017-03-29 20:52:16 +0200

    Added changelog, crafter; Fixed title duplication bug

--- ModItemTooltips.cs
+++ ModItemTooltips.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using Terraria;
 using Terraria.ModLoader;
@@ -8,10 +9,6 @@ using Terraria.ModLoader.IO;
 namespace ModItemTooltips
 {
 	// (c) Jofairden / Gorateron
-	// 18.2.2017
-	// Will have items from mods display their mod's displayname between brackets in their tooltip.
-	// Toggable by using '/moditemtooltips' in chat (saved with ModPlayer)
-	// Note: for now, will duplicate in item browsers
 
 	public class ModItemTooltips : Mod
 	{
@@ -26,20 +23,20 @@ namespace ModItemTooltips
 			};
 		}
 
-		public override void ChatInput(string text, ref bool broadcast)
-		{
-			if (text.Length > 1 && text[0] == '/')
-			{
-				string command = text.Substring(1);
-				if (command.ToUpper() == "MODITEMTOOLTIPS")
-				{
-					var mPlayer = Main.LocalPlayer.GetModPlayer<ModItemTooltipsPlayer>(this);
-					mPlayer.enabledTitleTT = !mPlayer.enabledTitleTT;
-					string toggleText = mPlayer.enabledTitleTT ? "on" : "off";
-					Main.NewText($"Toggled ModItemTooltips {toggleText}", 255, 255, 0);
-				}
-			}
-		}
+		//public override void ChatInput(string text, ref bool broadcast)
+		//{
+		//	if (text.Length > 1 && text[0] == '/')
+		//	{
+		//		string command = text.Substring(1);
+		//		if (command.ToUpper() == "MODITEMTOOLTIPS")
+		//		{
+		//			var mPlayer = Main.LocalPlayer.GetModPlayer<ModItemTooltipsPlayer>(this);
+		//			mPlayer.enabledTitleTT = !mPlayer.enabledTitleTT;
+		//			string toggleText = mPlayer.enabledTitleTT ? "on" : "off";
+		//			Main.NewText($"Toggled ModItemTooltips {toggleText}", 255, 255, 0);
+		//		}
+		//	}
+		//}
 
 		public override void Load()
 		{
@@ -72,20 +69,56 @@ namespace ModItemTooltips
 		}
 	}
 
+	public class ModItemTooltipInfo : ItemInfo
+	{
+		public static ModItemTooltipInfo GetInfo(Item item, Mod mod) =>
+			item.GetModInfo<ModItemTooltipInfo>(mod);
+
+		public string crafter = null;
+
+		public override ItemInfo Clone()
+		{
+			var clone = new ModItemTooltipInfo {crafter = crafter};
+			return clone;
+		}
+	}
+
 	public class ModItemTooltipsGlobalItem : GlobalItem
 	{
 		public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
 		{
 			if (!Main.LocalPlayer.GetModPlayer<ModItemTooltipsPlayer>(mod).enabledTitleTT || item.modItem == null) return;
 
-			var layer = tooltips.FirstOrDefault(x => x.mod == "Terraria"
-													 && x.Name == "ItemName");
+			var layer =
+				tooltips
+					.FirstOrDefault(x =>
+						x.mod == "Terraria"
+						&& x.Name == "ItemName");
 
 			string addedText = $" [{item.modItem.mod.DisplayName}]";
 
-			if (layer != null)
+			if (layer != null
+				&& !layer.text.EndsWith(addedText.Trim()))
 				layer.text += addedText;
 
+			var info = ModItemTooltipInfo.GetInfo(item, mod);
+
+			if (info.crafter != null)
+			{
+				tooltips.Insert(tooltips.IndexOf(layer), new TooltipLine(mod, "CrafterTooltip", $"Crafted by: {info.crafter}"));
+			}
+		}
+
+		public override void NetSend(Item item, BinaryWriter writer)
+		{
+			var info = ModItemTooltipInfo.GetInfo(item, mod);
+			writer.Write(info.crafter);
+		}
+
+		public override void NetReceive(Item item, BinaryReader reader)
+		{
+			var info = ModItemTooltipInfo.GetInfo(item, mod);
+			info.crafter = reader.ReadString();
 		}
 	}
 }
